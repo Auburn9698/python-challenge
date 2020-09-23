b05d99d1fb0d9f28f055c49efde67d9fd4aba93c
Auburn9698 <atkingn67@yahoo.com>
2020-09-23 12:15:56 -0400

    Added output
    Correcting small things, adding output to analysis.txt file

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,6 +16,7 @@ greatest_profit_month = 0
 greatest_loss = 0
 greatest_loss_month = 0
 initial_amount = 0
+month = []
 monthly_changes = []
 
 # Method 2: Improved Reading using CSV module
@@ -32,36 +33,53 @@ with open(csvpath) as csvfile:
         #Calculations for totals:
         total_months = total_months + 1
         total_amount = total_amount + int(row[1])
+        month.append(row[0])
 
         # Check to see if this is the greatest profit so far:
         if int(row[1]) > greatest_profit:
             greatest_profit = int(row[1])
             greatest_profit_month = row[0]
 
-        # Check to see if this is the greatest loss so far:        
+        # Check to see if this is the greatest loss so far:      
         if int(row[1]) < greatest_loss:
             greatest_loss = int(row[1])
             greatest_loss_month = row[0]
 
-        # Calculate the average monthly change amount:
-        monthly_amount = int(row[1])
-        monthly_change = monthly_amount - initial_amount
+        # For Calculating the average monthly change amount:
+        monthly_change = int(row[1]) - int(initial_amount)
         monthly_changes.append(monthly_change)
-        initial_amount = monthly_amount
-        average_change = sum(monthly_changes) / len(monthly_changes)
+        initial_amount = int(row[1])
+       
 
-        # Find the largest increase and decrease:
-        greatest_increase = max(monthly_changes)
-        greatest_decrease = min(monthly_changes)
+    #Calculate average change:
+    average_change = sum(monthly_changes) / total_months
+
+    # Find the largest increase and decrease:
+    greatest_increase = max(monthly_changes)
+    greatest_decrease = min(monthly_changes)
+
+    increase_month = month[monthly_changes.index(greatest_increase)]
+    decrease_month = month[monthly_changes.index(greatest_decrease)]
+        
 
 # Print the output:
-print("Financial Analysis")
-print("--------------------")
+print(" ")
+print("Financial Analysis:")
+print("-----------------------------------------------")
 print(f"Total Months: {total_months}")
-print(f"Net Total: {total_amount}")
-print(f"Average Change: {average_change}")
-print(f"Greatest Increase in Profits: {greatest_profit_month} ({greatest_increase})")
-print(f"Greatest Decrese in Losses: {greatest_loss_month} ({greatest_decrease})")
+print(f"Net Total: ${total_amount}")
+print(f"Average Change: ${average_change}")
+print(f"Greatest Increase in Profits: {increase_month} (${greatest_increase})")
+print(f"Greatest Decrese in Losses: {decrease_month} (${greatest_decrease})")
+
+with open(pathout, "w") as text_file:
+    text_file.write("Financial Analysis\n")
+    text_file.write("--------------------\n")
+    text_file.write(f"Total Months: {total_months}\n")
+    text_file.write(f"Net Total: ${total_amount}\n")
+    text_file.write(f"Average Change: ${average_change}\n")
+    text_file.write(f"Greatest Increase in Profits: {increase_month} (${greatest_increase})\n")
+    text_file.write(f"Greatest Decrese in Losses: {decrease_month} (${greatest_decrease})\n")
 
 
 
