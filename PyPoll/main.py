795c07323dbc763aa79a30ca334738bb12335168
Auburn9698 <atkingn67@yahoo.com>
2020-09-24 09:05:10 -0400

    Tried an error catcher
    Trying an error catcher in case some of the cells don't match any of the candidate's names exactly.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,6 +13,7 @@ Khan_votes = 0
 Correy_votes = 0
 Li_votes = 0
 OTooley_votes = 0
+Other_votes = 0
 Total_votes = 0
 
 # Method 2: Improved Reading using CSV module
@@ -37,11 +38,13 @@ with open(csvpath) as csvfile:
             Correy_votes = Correy_votes + 1
         elif (row[2] == "Li"):
             Li_votes = Li_votes + 1
-        else:
+        elif (row[2] == "O'Tooley"):
             OTooley_votes = OTooley_votes + 1
+        else:
+            Other_votes = Other_votes + 1
     
     # Compare Totals:
-    Winner = max(Khan_votes, Correy_votes, Li_votes, OTooley_votes)
+    Winner = max(Khan_votes, Correy_votes, Li_votes, OTooley_votes, Other_votes)
        
     #Setting name of winner:
     if Winner == Khan_votes:
@@ -50,14 +53,18 @@ with open(csvpath) as csvfile:
         Winning_Name = "Correy"
     elif Winner == Li_votes:
         Winning_Name = "Li"
-    else:
+    elif Winner == OTooley_votes:
         Winning_Name = "O'Tooley"
+    #Trying to add an error catch in case some cells don't match names exactly:
+    else:
+        Winning_name = "Other"
 
     # Calculate percentages for each candidate:
     Khan_percent = Khan_votes / Total_votes
     Correy_percent = Correy_votes / Total_votes
     Li_percent = Li_votes / Total_votes
     OTooley_percent = OTooley_votes / Total_votes
+    Other_percent = Other_votes / Total_votes
 
 #Printing Election Results:
 print("Election Results")
@@ -68,6 +75,7 @@ print(f"Khan: {Khan_percent:.2%} ({Khan_votes})")
 print(f"Correy: {Correy_percent: .2%} ({Correy_votes})")
 print(f"Li: {Li_percent:.2%} ({Li_votes})")
 print(f"O'Tooley: {OTooley_percent:.2%} ({OTooley_votes})")
+print(f"Other: {Other_percent:.2%} ({Other_votes})")
 print("----------------------------------------")
 print(f"Winner: {Winning_Name}")
 print("----------------------------------------")
