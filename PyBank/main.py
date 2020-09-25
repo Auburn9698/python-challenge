3f053b60b28a781ef4ac7aeb8dd954e6b3baf510
Auburn9698 <atkingn67@yahoo.com>
2020-09-25 10:32:19 -0400

    Finally corrected for Average Change
    Finally figured out how to fix the Average Change, while keeping Net Total and Total Months accurate.  Think all the calculations work at this point.
    Used the pop method to remove the first value from monthly_changes and month

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,10 +11,7 @@ pathout = os.path.join("analysis", "analysis.txt")
 # Create the variables:
 total_months = 0
 total_amount = 0
-greatest_profit = 0
-greatest_profit_month = 0
-greatest_loss = 0
-greatest_loss_month = 0
+previous_amount = 0
 month = []
 monthly_changes = []
 
@@ -26,8 +23,6 @@ with open(csvpath) as csvfile:
     
     # Read The Header Row First (Skip This Step If There Is No Header)
     csv_header = next(csvreader)
-    row = next(csvreader)  #For skipping first month that messed with avg change.
-    previous_amount = int(row[1])
 
     # Loop through file, calculate basic totals:
     for row in csvreader:
@@ -35,30 +30,19 @@ with open(csvpath) as csvfile:
         total_amount = total_amount + int(row[1])
         # Add to the months list, used later for month indexing
         month.append(row[0])
-
-        # Check to see if this is the greatest profit so far:
-        if int(row[1]) > greatest_profit:
-            greatest_profit = int(row[1])
-            greatest_profit_month = row[0]
-
-        # Note: Not sure I needed largest-profit or largest-loss.
-        # It looked like the homework was asking for that at first,
-        # but I think it was actually looking for the greatest changes
-        # +/- from month to month.  Wasn't real clear at first.
-
-        # Check to see if this is the greatest loss so far:      
-        if int(row[1]) < greatest_loss:
-            greatest_loss = int(row[1])
-            greatest_loss_month = row[0]
-
         # For Calculating the average monthly change amount:
         monthly_change = int(row[1]) - previous_amount
         monthly_changes.append(monthly_change)
         previous_amount = int(row[1])
-       
+
+
+    #Remove the first value from monthly_changes, since there was no previous month to subtract:
+    monthly_changes.pop(0)
+    #Remove the first value from the months, same reason as above:
+    month.pop(0)
 
     # Calculate average change:
-    average_change = sum(monthly_changes) / total_months
+    average_change = sum(monthly_changes) / (total_months -1)
 
     # Find the largest increase and decrease:
     greatest_increase = max(monthly_changes)
@@ -67,7 +51,7 @@ with open(csvpath) as csvfile:
     increase_month = month[monthly_changes.index(greatest_increase)]
     decrease_month = month[monthly_changes.index(greatest_decrease)]
 
-# Storing output in a tuple to print and write to text file(shorter code)        
+# Storing output in a tuple to print and write to text file:        
 output = (
     " \n"
     "Financial Analysis: \n"
